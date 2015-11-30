3a172b639b59a38eeab69505e92ecaad9b206756
Oskar Emilsson <oskaremilsson@gmail.com>
2015-11-30 16:39:17 +0100

    More styling

--- examination/client/source/js/app.js
+++ examination/client/source/js/app.js
@@ -2,11 +2,28 @@
 var Quiz = require("./Quiz");
 var q;
 
+function addThemeSelector() {
+    //add listener for the theme chooser
+    var select = document.querySelector("#theme-selector");
+    select.addEventListener("change", function() {
+        var baseStyle = document.querySelector("#baseStyle");
+        var loadingStyle = document.querySelector("#loadingStyle");
+        localStorage.setItem("theme", select.value);
+        if(select.value === "playful") {
+            baseStyle.setAttribute("href", "stylesheet/playful.css");
+            loadingStyle.setAttribute("href", "stylesheet/playful_loading.css");
+        }
+        else if(select.value === "hacker") {
+            baseStyle.setAttribute("href", "stylesheet/hacker.css");
+            //loadingStyle.setAttribute("href", "stylesheet/hacker_loading.css");
+        }
+    });
+}
+
 /**
  * Function to handle the submit for nickname and start the quiz
  * @param event, the eventhandler from the listener
  */
-
 function submit(event) {
     if (event.which === 13 || event.keyCode === 13 || event.type === "click") {
         //disable forms action so page wont reload with enter
@@ -27,21 +44,7 @@ var form = document.querySelector("#qForm");
 button.addEventListener("click",submit, true);
 form.addEventListener("keypress", submit, true);
 
-//add listener for the theme chooser
-var select = document.querySelector("#theme-selector");
-select.addEventListener("change", function() {
-    var baseStyle = document.querySelector("#baseStyle");
-    var loadingStyle = document.querySelector("#loadingStyle");
-    localStorage.setItem("theme", select.value);
-    if(select.value === "playful") {
-        baseStyle.setAttribute("href", "stylesheet/playful.css");
-        loadingStyle.setAttribute("href", "stylesheet/playful_loading.css");
-    }
-    else if(select.value === "hacker") {
-        baseStyle.setAttribute("href", "stylesheet/hacker.css");
-        //loadingStyle.setAttribute("href", "stylesheet/hacker_loading.css");
-    }
-});
+addThemeSelector();
 
 if(localStorage.getItem("theme")) {
     var theme = localStorage.getItem("theme");
